a86788a20f6a543b706f509c9a02a019605b0617
nazufel <nazufel@protonmail.com>
2023-02-15 16:00:45 -0500

    sign up modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,9 +34,20 @@ export default function Home() {
 							<Link href="/about">
 								<button className="btn btn-primary mr-2">About Me</button>
 							</Link>
-							<Link href="">
-								<button className="btn btn-secondary ml-2">Newsletter Sign Up</button>
-							</Link>
+							{/* The button to open modal */}
+							<label htmlFor="my-modal" className="btn btn-secondary">NewsLetter Sign Up</label>
+
+							{/* Put this part before </body> tag */}
+							<input type="checkbox" id="my-modal" className="modal-toggle" />
+							<div className="modal">
+								<div className="modal-box">
+									<h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+									<p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+									<div className="modal-action">
+										<label htmlFor="my-modal" className="btn">Yay!</label>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
