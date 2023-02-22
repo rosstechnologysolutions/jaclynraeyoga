97d589bbb5057c6ac4629586bc17aec1f5364680
nazufel <nazufel@proton.me>
2023-02-22 14:49:52 -0500

    added breakpoints to the navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,28 +9,27 @@ export default function NavBar() {
   return (
     <>
       {/* NAV */}
-      {/* TODO: center jaclyn rae in the nav bar, not in the cols*/}
       {/* TODO: resize font on mobile*/}
       <div className="navbar bg-accent">
-      <div className="flex-1">
-      <Link href="/">
-          <Image
-          	alt="logo for jaclyn rae yoga"
-            src={Logo}
-            width={100}
-            height={100}
-            >
-          </Image>
-        </Link>
+        <div className="flex-1">
+        <Link className="invisible sm:visible" href="/">
+            <Image
+              alt="logo for jaclyn rae yoga"
+              src={Logo}
+              width={100}
+              height={100}
+              >
+            </Image>
+          </Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1 invisible sm:visible">
+            <li className="text-xl font-bold"><Link href="/">Home</Link></li>
+            <li className="text-xl font-bold"><Link href="/about">About</Link></li>
+            <li className="text-xl font-bold"><Link href="/schedule">Schedule</Link></li>
+          </ul>
+        </div>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li className="text-xl font-bold"><Link href="/">Home</Link></li>
-					<li className="text-xl font-bold"><Link href="/about">About</Link></li>
-					<li className="text-xl font-bold"><Link href="/schedule">Schedule</Link></li>
-        </ul> 
-      </div>
-    </div>
     </>
   )
 }
