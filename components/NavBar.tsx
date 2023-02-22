fdb707fc918aa8d9f4f379e87e82e8ea37725c7a
nazufel <nazufel@protonmail.com>
2023-02-22 14:28:42 -0500

    fixed the nav bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,8 +11,9 @@ export default function NavBar() {
       {/* NAV */}
       {/* TODO: center jaclyn rae in the nav bar, not in the cols*/}
       {/* TODO: resize font on mobile*/}
-      <div className="navbar bg-accent grid grid-flow-col grid-cols-3">
-        <Link href="/">
+      <div className="navbar bg-accent">
+      <div className="flex-1">
+      <Link href="/">
           <Image
           	alt="logo for jaclyn rae yoga"
             src={Logo}
@@ -20,18 +21,16 @@ export default function NavBar() {
             height={100}
             >
           </Image>
-            </Link>
-              <div className="">
-              	<a className="normal-case text-5xl text-base-200 font-bold drop-shadow-md content-center">Jaclyn Rae Yoga</a>
-            	</div>
-        <div className="flex-none">
-					<ul className="menu menu-horizontal px-1 content-end">
-						<li className="text-xl font-bold"><Link href="/">Home</Link></li>
-						<li className="text-xl font-bold"><Link href="/about">About</Link></li>
-						<li className="text-xl font-bold"><Link href="/schedule">Schedule</Link></li>
-					</ul>
-				</div>
+        </Link>
       </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li className="text-xl font-bold"><Link href="/">Home</Link></li>
+					<li className="text-xl font-bold"><Link href="/about">About</Link></li>
+					<li className="text-xl font-bold"><Link href="/schedule">Schedule</Link></li>
+        </ul>
+      </div>
+    </div>
     </>
   )
 }
