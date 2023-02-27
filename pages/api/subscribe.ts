85a24352f7467db5481a37b079646668cbeff07f
nazufel <nazufel@proton.me>
2023-02-27 01:27:15 -0500

    working

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -22,16 +22,17 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 	
 		console.log(subscriber)
 	
-		const response = subscribe(subscriber)
+		subscribe(subscriber).then((response) =>{
+			console.log(response.status)
+		});
 	
 		// res.status(200).write(response)
 	
 		res.status(200).json({ subscribed: true })
 	}
 
-
-const subscribe = (subscriber: SubscriberProps) => {
-	fetch ('https://' + process.env.MAILCHIMP_SERVER_PREFIX + '.api.mailchimp.com/3.0/lists/' + process.env.MAILCHIMP_LIST_ID + '/members/', {
+async function subscribe(subscriber: SubscriberProps) {
+	const response = await fetch ('https://' + process.env.MAILCHIMP_SERVER_PREFIX + '.api.mailchimp.com/3.0/lists/' + process.env.MAILCHIMP_LIST_ID + '/members/', {
 		body: JSON.stringify({
 			'email_address': subscriber.email,
 			'status': 'subscribed',
@@ -45,8 +46,33 @@ const subscribe = (subscriber: SubscriberProps) => {
 			"Authorization": "Basic " + new Buffer('any:' + process.env.MAILCHIMP_API_KEY ).toString('base64')
 		},
 		method: "POST",
-	})
+	});
 
-	.then((response) => response.json())
-	.then((json) => return json)
+	// console.log(response.json)
+	return response.json();
+
+	// .then((response) => response.json())
+	// .then((json) => console.log(json))
 };
+
+// const subscribe = (subscriber: SubscriberProps) => {
+// 	const response = fetch ('https://' + process.env.MAILCHIMP_SERVER_PREFIX + '.api.mailchimp.com/3.0/lists/' + process.env.MAILCHIMP_LIST_ID + '/members/', {
+// 		body: JSON.stringify({
+// 			'email_address': subscriber.email,
+// 			'status': 'subscribed',
+// 			'merge_fields': {
+// 				'FNAME': subscriber.firstName,
+// 				'LNAME': subscriber.lastName
+// 			}
+// 		}),
+// 		headers: {
+// 			"Content-Type": "application/json;charset=utf-8",
+// 			"Authorization": "Basic " + new Buffer('any:' + process.env.MAILCHIMP_API_KEY ).toString('base64')
+// 		},
+// 		method: "POST",
+// 	})
+
+// 	.then((response) => response.json())
+// 	.then((json) => console.log(json))
+// };
+
