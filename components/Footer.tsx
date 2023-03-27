207b28d6fcbe63487fb10be47845f5b7b1d49982
nazufel <nazufel@proton.me>
2023-03-27 15:40:40 -0400

    added privacy policy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,13 @@
-
+import Link from 'next/link'
 
 export default function Footer() {
 
 	return (
 		<>
 			<footer className="footer footer-center p-4 bg-base-300 text-base-content mt-auto">
-				<div>
-					<p>Copyright © {new Date().getFullYear()} - All rights reserved by Jaclyn Ross</p>
+				<div className="flex flex-col">
+					<p>Copyright © {new Date().getFullYear()} - All rights reserved by Jaclyn Ross | <Link href="/privacy">Privacy Policy</Link></p>
+					<p>Site designed, built, and hosted by <Link href="https://rosstechnologysolutions.com" target="_blank">Ross Technology Solutions LLC</Link></p>
 				</div>
 			</footer>
 		</>
---
+++ pages/privacy.tsx
@@ -0,0 +1,67 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="container my-10">
+      <Head>
+      <title>Jaclyn Rae Yoga | Privacy</title>
+      </Head>
+      <h2 className="text-xl"><b>Updated: 03/27/2023</b></h2>
+      <br />
+      <p><Link className="underline" href="https://rosstechnologysolutions.com" target="_blank">Ross Technology Solutions LLC</Link> operates the jaclynraeyoga.com website, which provides the SERVICE.
+
+        This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the Jaclyn Rae Yoga website.
+
+        If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy. Our Privacy Policy was created with the help of the Privacy Policy Template Generator.
+
+        The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Jaclyn Rae Yoga.com, unless otherwise defined in this Privacy Policy.</p>
+
+        <br />
+        <h2 className="text-xl font-bold">Information Collection and Use</h2>
+
+        <p>The Service does not automatically collection any information while you use the Service. The only peronal identifying information collected is from the voluntary Contact Form. This information you provide will be used to contact you. Contact Form information includs, but is not limited to your name, email, company name, and phone number</p>
+
+        <br />
+        <h2 className="text-xl font-bold">Log Data</h2>
+        <p>We want to inform you that whenever you visit our Service, we do not collect browser information.</p>
+
+        <br />
+        <h1 className="text-xl font-bold">Cookies</h1>
+        <p>Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your computer’s hard drive. Our website does not uses these "cookies" to collection information and to improve our Service.</p>
+
+        <p>For more general information on cookies, please read "Cookies" article from the Privacy Policy Generator.</p>
+
+        <br />
+        <h2 className="text-xl font-bold">Service Providers</h2>
+        <p>We may employ third-party companies and individuals due to the following reasons:</p>
+          <ul>
+            <li>To facilitate our Service</li>
+            <li>To provide the Service on our behalf</li>
+            <li>To perform Service-related services</li>
+            <li>To assist us in analyzing how our Service is used</li>
+          </ul>
+        <p>We want to inform our Service users that these third parties have access to your Personal Information you submitted in the Contact Form. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.</p>
+
+        <br />
+        <h2 className="text-xl font-bold">Security</h2>
+        <p>We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
+
+        <br />
+        <h2 className="text-xl font-bold">Links to Other Sites</h2>
+        <p>Our Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over, and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
+
+        <br />
+        <h2 className="text-xl font-bold">Children's Privacy</h2>
+        <p>Our Services do not address anyone under the age of 13. We do not knowingly collect personal identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.</p>
+
+        <br />
+        <h2 className="text-xl font-bold">Changes to This Privacy Policy</h2>
+        <p>We may update our Privacy Policy from time to time. Thus, we advise you to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately, after they are posted on this page.</p>
+
+        <br />
+        <h2 className="text-xl font-bold">Contact Us</h2>
+        <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.</p>    
+    </div>
+  )
+}
