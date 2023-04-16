31cd92d8f5f69b0a9bbacd4bf9ba119fe21e6143
nazufel <nazufel@proton.me>
2023-04-16 15:49:50 -0400

    typo

--- components/OptinBottomBanner.tsx
+++ components/OptinBottomBanner.tsx
@@ -7,7 +7,7 @@ export default function OptInBottomBanner() {
 			{/* Playlist Call to Action */}
 			<section>
 				<div className="bg-gradient-to-r from-accent to-accent-focus my-10">
-					<h3 className="text-md font-bold md:font-normal md:text-4xl text-center p-4">Sign Up For Your Free Prental Playlist on Spotify</h3>
+					<h3 className="text-md font-bold md:font-normal md:text-4xl text-center p-4">Sign Up For Your Free Prenatal Playlist on Spotify</h3>
 					<div className="flex flex-row justify-center p-1 pb-5">
 					<Link href="https://upbeat-leader-1917.ck.page/43b949eaf3">
 						<button className="btn btn-primary">Sign Up for the Link</button>
